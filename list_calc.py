33ade092e4ade85beb02bed55ce04ad9acc12422
secrierum <dqq02@proton.me>
2023-09-18 11:42:43 +0300

    Unused list was deleted

--- list_calc.py
+++ list_calc.py
@@ -3,8 +3,6 @@ Define a function sum() and a function multiply() that sums and multiplies
 should return 10, and multiply([1, 2, 3, 4]) should return 24.
 '''
 
-li = [1, 2, 3, 4]
-
 
 def sum(x):
     s = 0
