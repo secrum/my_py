7dd2de614db95813588909f18957291295d819e2
secrierum <dqq02@proton.me>
2023-09-24 01:49:06 +0300

    Unneded comments deleted

--- overlapping.py
+++ overlapping.py
@@ -7,20 +7,9 @@ write it using two nested for-loops.
 
 from member import is_member
 
+
 def overlapping(lst1, lst2):
     for itm in lst1:
         if is_member(itm, lst2):
             return True
     return False
-        
-
-    #     for itm2 in lst2:
-    #         if itm1 == itm2:
-    #             return True
-    # return False
-
-# def is_member2(itm, lst)jjjjjj
-#     for elem in lst:
-#         if elem == itm:
-#             return True
-#     return False
